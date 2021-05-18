ec5cd3580df1c1236e7b7794543a3edbb898f2c4
liqm <liqm@lenovocloud.com>
2021-05-18 13:33:05 +0800

    feat: useMemo when need the performance benefits

--- src/UseMemoDemo.jsx
+++ src/UseMemoDemo.jsx
@@ -1,5 +1,9 @@
-import React, {useState, useMemo} from 'react'
-
+import React, {useState, useMemo, useEffect} from 'react'
+/*
+* get
+*  有助于避免在每次渲染时都进行高开销的计算：useMemo 传入创建函数和依赖函数项，它仅会在某个依赖项改变时才重新计算memoized值；
+*  scenarios ：you actually need the performance benefits when the function you're calling is incredibly slow
+* */
 function slowFunction(number) {
     console.log('call slow function');
     for (let i = 0; i <= 1000000; i++) {
@@ -10,7 +14,10 @@ function slowFunction(number) {
 export default function () {
     const [number, setNumber] = useState(0)
     const [theme, setTheme] = useState('dark')
-    const doubleNumber = () => slowFunction(number)
+    const themeObj = {
+        background: theme === 'dark'?'#ccc':'#eee'
+    }
+    const doubleNumber = () => useMemo(()=>slowFunction(number),[number])
     return (
         <div>
             <input type="number" onChange={(e) => {
