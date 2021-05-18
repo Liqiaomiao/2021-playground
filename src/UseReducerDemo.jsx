6ca3a00c470f9702bb34442171f07773244b3088
liqm <liqm@lenovocloud.com>
2021-05-18 19:58:15 +0800

    fix: error fix

--- src/UseReducerDemo.jsx
+++ src/UseReducerDemo.jsx
@@ -1,27 +1,26 @@
-import React, {useReducer} from 'react'
-
-const reducer = (state, action, c) => {
+import React,{useReducer} from 'react'
+const reducer = (state,action)=>{
     const {count} = state
-    switch (action.type) {
+    switch (action.type){
         case 'increment':
-            return {count: count + 1}
+           return { count: count+1 }
         case 'decrement':
-            return {count: count - 1}
+            return { count: count-1}
             break;
         default:
-            return payload
+            return { count }
     }
 }
-export default function () {
-    const [state, dispatch] = useReducer(reducer, {
+export default function (){
+    const [state,dispatch] = useReducer(reducer,{
         count: 0
     })
 
     return (
         <>
-            <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+           <button onClick={()=>dispatch({type: 'decrement'})}>-</button>
             <span>{state.count}</span>
-            <button onClick={() => dispatch({type: 'increment'})}>+</button>
+            <button onClick={()=>dispatch({type: 'increment'})}>+</button>
         </>
     )
 }
