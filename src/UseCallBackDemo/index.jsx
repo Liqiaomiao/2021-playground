a41a37ae64582ded143d4a8b61135147464c6405
liqm <liqm@lenovocloud.com>
2021-05-18 08:25:19 +0800

    feat: use useCallback

--- src/UseCallBackDemo/index.jsx
+++ src/UseCallBackDemo/index.jsx
@@ -1,11 +1,18 @@
-import React,{useState} from 'react'
+import React,{useState,useCallback} from 'react'
 import List from './list'
+/*
+* get
+* useCallback 返回该回调函数的 memoized 版本，该回调函数仅在某个依赖项改变时才会更新
+* */
 export default function UseCallBackDemo(){
     const [number,setNumber] = useState(1)
     const [theme,setTheme] = useState('dark')
     const getItems = ()=>{ // 任何更新都会重新render, 并且每次都创建新的 getItems
         return [number,number+1,number+2]
     }
+    const getItemsMemo = useCallback(()=>{ // 当number改变，该函数才更新
+        return [number,number+1,number+2]
+    },[number])
     const themeObj = {
         'dark': {
             background: '#ccc'
@@ -22,7 +29,7 @@ export default function UseCallBackDemo(){
             }/>
             <button onClick={()=>setTheme(prev=>prev==='dark'?'light':'dark')}>Toggle Theme</button>
             {
-                <List getItems={getItems}/>
+                <List getItems={getItemsMemo}/>
             }
         </div>
     )
