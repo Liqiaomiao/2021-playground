901f5a9665e14b4c281430396e7aa0d3b32e0846
liqm <liqm@lenovocloud.com>
2021-05-19 20:06:48 +0800

    feat: redux with toolkit

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import {increment,decrement,incrementByAmount} from './features/counter-with-toolkit/counter'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch()
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h1>The count is:{count} </h1>
+                <button onClick={()=>dispatch(increment())}>+</button>
+                <button onClick={()=>dispatch(decrement())}>-</button>
+                <button onClick={()=>dispatch(incrementByAmount(5))}>add by amount</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
